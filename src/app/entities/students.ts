40fd09599891d729fb5f1768d11ea5cfe41346de
Empresaurio <kendallcorralessanchez@gmail.com>
2026-03-03 23:00:44 +0100

    Feat: student-register frontend

---
+++ src/app/entities/students.ts
@@ -0,0 +1,10 @@
+export class Student{
+
+    name: string = ""
+    lastName: string = ""
+    mail: string = ""
+    password: string = ""
+    birthdate: Date = new Date(0, 0, 0)
+    province: string = ""
+    carrerInterest: string = ""
+}
--- src/app/pages/company-register.component/company-register.component.ts
+++ src/app/pages/company-register.component/company-register.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-company-register.component',
-  imports: [],
+  imports: [ButtonModule, StepperModule],
   templateUrl: './company-register.component.html',
   styleUrl: './company-register.component.css',
 })
