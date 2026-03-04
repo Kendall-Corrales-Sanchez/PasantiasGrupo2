d49a29d6dd54ce73f7422f7f0c79b0ac07cac966
Empresaurio <kendallcorralessanchez@gmail.com>
2026-03-05 00:22:50 +0100

    Feat: Company register

---
+++ src/app/entity/Companies.ts
@@ -0,0 +1,9 @@
+export class Companies{
+
+    nameCompany: string = ""
+    mail: string = ""
+    password: string = ""
+    province: string = ""
+    WhoAreWe: string = ""
+    sectorType: string = ""
+}
