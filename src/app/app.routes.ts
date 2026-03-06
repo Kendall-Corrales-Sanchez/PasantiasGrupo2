55cb949851eb8c40918ac760d6a9b115ca109aa2
Empresaurio <kendallcorralessanchez@gmail.com>
2026-03-06 22:04:37 +0100

    Feat: Layout Side

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Register } from './pages/register/register';
 import { ChooseRoleComponent } from './pages/choose-role.component/choose-role.component';
 import { CompanyRegisterComponent } from './pages/company-register.component/company-register.component';
 import { StudentRegisterComponent } from './pages/student-register.component/student-register.component';
+import { MenuLateral } from './pages/menu-lateral-student/menu-lateral-student';
+import { Layout } from './pages/layout/layout';
 
 export const routes: Routes = [
     {
@@ -30,4 +32,22 @@ export const routes: Routes = [
         path: 'registerStudent',
         component: StudentRegisterComponent
     },
+    {
+        path: 'menu',
+        component: MenuLateral
+    },
+
+    {
+    path: 'menus',
+    component: Layout,
+    children: [
+      { path: 'principal', loadComponent: () => Login },
+      { path: 'perfil',    loadComponent: () => Register },
+      { path: 'mensajeria', loadComponent: () => CompanyRegisterComponent },
+      { path: 'notificaciones', loadComponent: () => ChooseRoleComponent },
+      { path: '', redirectTo: 'principal', pathMatch: 'full' },
+    ],
+  },
+
+  { path: '**', redirectTo: 'login' },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { Footer } from "./pages/footer/footer";
+import { MenuLateral } from "./pages/menu-lateral-student/menu-lateral-student";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, RouterLinkWithHref],
+  imports: [RouterOutlet, Footer, RouterLinkWithHref, MenuLateral],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
