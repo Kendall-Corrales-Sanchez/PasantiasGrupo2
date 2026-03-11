653a6d028d2683799b82fb5ba5a37b6c90a19f36
Empresaurio <kendallcorralessanchez@gmail.com>
2026-03-11 18:15:14 +0100

    Breaking Change: Interships

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), providePrimeNG({
             theme: {
-                preset: Aura
+                preset: Lara
             }
         })
   ]
